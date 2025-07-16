92c56658fe8eef318b8cd9956edf0347c5c26657
Lars <lars@serlo.org>
2025-07-16 11:51:18 +0200

    feat: add test checking the Serlo Editor test article

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -25,3 +25,16 @@ describe("https://de.serlo.org/auth/login", () => {
     cy.url({ timeout: 10000 }).should("eq", "https://de.serlo.org/");
   });
 });
+
+describe("Serlo Editor test article", () => {
+  it("opens without error", () => {
+    cy.visit("https://de.serlo.org/auth/login");
+    cy.get('input[name="identifier"]').type("LarsTest");
+    cy.get('input[name="password"]').type(Cypress.env("SERLO_ORG_PASSWORD"));
+    cy.contains("button", "Anmelden").click();
+    // User gets redirected to home on successful login
+    cy.url({ timeout: 10000 }).should("eq", "https://de.serlo.org/");
+    cy.visit("https://de.serlo.org/entity/repository/add-revision/277232");
+    cy.get(".plugin-type-article");
+  });
+});
