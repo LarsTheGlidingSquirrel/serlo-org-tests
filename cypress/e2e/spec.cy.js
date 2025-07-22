62aa637f5a71a56918fce713e7461b7f6d972dd1
Lars <lars@serlo.org>
2025-07-22 09:54:55 +0200

    feat: use cypress command for login

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -12,29 +12,42 @@ describe("https://serlo.org/", () => {
   });
 });
 
-describe("https://de.serlo.org/auth/login", () => {
-  beforeEach(() => {
-    cy.visit("https://de.serlo.org/auth/login");
-  });
-
-  it("allows user to log in", () => {
-    cy.get('input[name="identifier"]').type("LarsTest");
-    cy.get('input[name="password"]').type(Cypress.env("SERLO_ORG_PASSWORD"));
-    cy.contains("button", "Anmelden").click();
-    // User gets redirected to home on successful login
-    cy.url({ timeout: 10000 }).should("eq", "https://de.serlo.org/");
+it("User can log in", () => {
+  cy.loginOrReuseSession({
+    user: "LarsTest",
+    password: Cypress.env("SERLO_ORG_PASSWORD"),
+    expectSuccess: true,
   });
 });
 
-describe("Serlo Editor test article", () => {
-  it("opens without error", () => {
-    cy.visit("https://de.serlo.org/auth/login");
-    cy.get('input[name="identifier"]').type("LarsTest");
-    cy.get('input[name="password"]').type(Cypress.env("SERLO_ORG_PASSWORD"));
-    cy.contains("button", "Anmelden").click();
-    // User gets redirected to home on successful login
-    cy.url({ timeout: 10000 }).should("eq", "https://de.serlo.org/");
+it("User can't login with incorrect password", () => {
+  cy.loginOrReuseSession({
+    user: "LarsTest",
+    password: "incorrect-password",
+    expectSuccess: false,
+  });
+});
+
+describe("Logged in users can", () => {
+  beforeEach(() => {
+    cy.loginOrReuseSession({
+      user: "LarsTest",
+      password: Cypress.env("SERLO_ORG_PASSWORD"),
+      expectSuccess: true,
+    });
+  });
+  it("open Serlo Editor", () => {
     cy.visit("https://de.serlo.org/entity/repository/add-revision/277232");
     cy.get(".plugin-type-article");
+  });
+});
+
+describe("Non-logged in users can", () => {
+  it("not open Serlo Editor", () => {
+    cy.visit("https://de.serlo.org/entity/repository/add-revision/277232");
+    // Link to login is shown
+    cy.get('a[href="/auth/login"]').should("exist");
+    // Serlo Editor is not shown
+    cy.get(".plugin-type-article").should("not.exist");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,24 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Login or reuse existing session
+Cypress.Commands.add(
+  "loginOrReuseSession",
+  ({ user, password, expectSuccess }) => {
+    cy.session([user, password], () => {
+      cy.visit("https://de.serlo.org/auth/login");
+      cy.get('input[name="identifier"]').type(user);
+      cy.get('input[name="password"]').type(password);
+      cy.contains("button", "Anmelden").click();
+      if (expectSuccess) {
+        cy.url({ timeout: 10000 }).should("eq", "https://de.serlo.org/");
+      } else {
+        // User stays on login page on failed login
+        cy.url({ timeout: 10000 }).should(
+          "eq",
+          "https://de.serlo.org/auth/login"
+        );
+        cy.contains(
+          "Der Benutzername, die E-Mail-Adresse oder das Passwort stimmen so nicht. Bitte überprüfe deine Eingabe."
+        );
+      }
+    });
+  }
+);
